6cdf1830b04b56ffcb7d9a8fef8cc8e9bca9791e
杨晓锋 <14212078@bjtu.edu.cn>
2026-03-29 20:50:46 +0800

    chore: 清理测试输出目录

--- tests/output/temp_batch_34004_91444.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"D:\MyCode\IronCScript\ironcscript-core\build\IronCScriptCLI.exe" "D:\MyCode\IronCScript\ironcscript-core\tests\scripts\09_stdlib/stdio/test_scanf_basic.c" < "D:\MyCode\IronCScript\ironcscript-core\tests\inputs\test_scanf_basic_default.txt" > "D:\MyCode\IronCScript\ironcscript-core\tests\output\test_output_1.txt" 2>&1
-echo Exit code: %errorlevel% >> "D:\MyCode\IronCScript\ironcscript-core\tests\output\test_output_1.txt"
--- tests/output/temp_batch_44896_93900.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"D:\MyCode\CScriptEngine\build\CScriptEngine.exe" "D:\MyCode\CScriptEngine\tests\scripts\test_scanf_single.c" < "D:\MyCode\CScriptEngine\tests\inputs\test_int_100_200_300.txt" > "D:\MyCode\CScriptEngine\tests\output\test_output_78.txt" 2>&1
-echo Exit code: %errorlevel% >> "D:\MyCode\CScriptEngine\tests\output\test_output_78.txt"
